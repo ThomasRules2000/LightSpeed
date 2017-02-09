450ae7ae2cd90bc9181b18d3056a44aba3ac05d0
ThomasRules2000 <tstacey2000@gmail.com>
2017-02-09 23:17:06 +0000

    Basic Code for Tile Creation
    Created Tiles, prefabs, and script to convert int array to ship

---
+++ Assets/Scripts/ShipBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBase : MonoBehaviour
+{
+    //In plan, 0=empty, 1=floor, 2=wall
+    public int[,] plan;
+    public Transform floor;
+    public Transform wall;
+
+    public ShipBase(int[,] _plan, Transform _floor, Transform _wall)
+    {
+        plan = _plan;
+        floor = _floor;
+        wall = _wall;
+    }
+    public void spawn()
+    {
+        for(int x = 0; x<plan.GetLength(0); x++)
+        {
+            //int xPos = (int)transform.position.x + x - plan.GetLength(0) / 2;
+            for (int y=0; y<plan.GetLength(1); y++)
+            {
+                //int yPos = (int)transform.position.y + y - plan.GetLength(1) / 2;
+                Transform obj;
+                switch(plan[x, y])
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        obj = Instantiate(floor, new Vector3(x, 0, y), Quaternion.identity);
+                        //obj.SetParent(transform, false);
+                        break;
+                    case 2:
+                        obj = Instantiate(wall, new Vector3(x, 0, y), Quaternion.identity);
+                        //obj.SetParent(transform, false);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Taurus.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taurus : MonoBehaviour {
+
+    int[,] plan =
+        {
+            {0,0,0,0,0,0,1,1,0,0,0,0,0,0,0 },
+            {0,1,1,1,1,2,1,1,1,1,0,0,0,0,0 },
+            {1,1,1,2,1,1,1,1,1,1,1,1,1,1,1 },
+            {1,1,1,2,1,1,1,1,1,1,1,1,1,1,1 },
+            {0,1,1,1,1,2,1,1,1,1,0,0,0,0,0 },
+            {0,0,0,0,0,0,1,1,0,0,0,0,0,0,0 }
+        };
+
+    public Transform floor;
+    public Transform wall;
+
+	void Start () {
+        ShipBase taurus = new ShipBase(plan, floor, wall);
+        taurus.spawn();
+	}
+	
+}
